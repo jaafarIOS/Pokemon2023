3af9da83cf91f122d2385eb8388cde4382860614
Jaafar Zubaidi <jaafarwerdi@proton.me>
2023-01-25 19:34:34 -0500

    Initial Commit

---
+++ Pokemon2023/Pokemon2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon2023App.swift
+//  Pokemon2023
+//
+//  Created by Jaafar Zubaidi  on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
